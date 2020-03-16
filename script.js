51f54c05dd728c43c5b00ecac4088b6518228d25
Jimmy Smith <jasmith.188@gmail.com>
2020-03-16 16:08:17 -0400

    index

--- script.js
+++ script.js
@@ -1,29 +1,74 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//define variables
+
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var special = '!@#$^&%*()+=-[]{}|:<>?,.';
+var numbers = '1234567890';
+
+var pwd = '';
+
+var lowerSelection = false;
+var upperSelection = false;
+var specialSelection = false;
+var numberSelection = false;
 
 
+//function to generate a random password
 
-function generatePassword() {
+function generate() {
+    var confirmLength = '';
+//asking user to input desired character length
+    while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
+        confirmLength = prompt("What length would you like the password to be? (Between 8 to 128 characters)");
+        if (confirmLength === null) {
+            break;
+        }
+    }
+//getting user input for which character types to use for the password
+    if (confirmLength) {
+        if (confirm("Would you like to use lowercase characters?") == true) {
+            lowerSelection = true
+        } 
 
+        if (confirm("Would you like to use uppercase characters?") == true) {
+            upperSelection = true
+        }
 
-  var numChar = prompt("how many characters would you like to use?");
-  console.log(numChar);
-  
+        if (confirm("Would you like to use special characters?") == true) {
+            specialSelection = true
+        }
 
+        if (confirm("Would you like to use numerical characters?") == true) {
+            numberSelection = true
+        }
+//if none of the character types are selected, alerts the user to choose at least one
+        if (lowerSelection === false && upperSelection === false && specialSelection === false && numberSelection === false) {
+            alert("At least one character type must be selected")
+        }
+    }
+//generate random password
+    var characters = '';
+    characters += (lowerSelection ? lower : '');
+    characters += (upperSelection ? upper : '');
+    characters += (specialSelection ? special : '');
+    characters += (numberSelection ? numbers : '');
 
+    pwd = password(confirmLength, characters);
 
+    document.getElementById("password").innerHTML = pwd;
 
-  return "This is the password"
 }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function password(l, characters) {
+    var pwd = '';
+    for (var i = 0; i < l; ++i) {
+        pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pwd;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//function to copy password to computer clipboard
+function copied() {
+    document.getElementById("password").select();
+    document.execCommand("copy");
+    alert("The password has been copied to your clipboard!");
+}
