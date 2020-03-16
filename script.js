54d24f6934ad58b370934e683c2faa58c73984a7
Jimmy Smith <jasmith.188@gmail.com>
2020-03-16 15:25:14 -0400

    index

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+function generatePassword() {
+
+
+  var numChar = prompt("how many characters would you like to use?");
+  console.log(numChar);
+  
+
+
+
+
+  return "This is the password"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
